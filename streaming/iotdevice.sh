253f49ae2ab5c7eb3977397f683bec5946568aeb
Graeme Malcolm <gmalc@microsoft.com>
2021-10-22 15:04:48 -0700

    updated message count

--- streaming/iotdevice.sh
+++ streaming/iotdevice.sh
@@ -2,4 +2,4 @@ iothub=`cat iothub.txt`
 iotdevice=`cat iotdevice.txt`
 
 
-az iot device simulate --device-id $iotdevice --hub-name $iothub --properties "key0=data;key1=$(( $RANDOM % 113 + 1 ))" --msg-count 100 --msg-interval 3;
+az iot device simulate --device-id $iotdevice --hub-name $iothub --data "IOT Device Message" --properties "key0=$(( $RANDOM % 113 + 1 ));key1=$(( $RANDOM % 113 + 1 ))" --msg-count 120 --msg-interval 1;
